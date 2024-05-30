774e9ed46cd873585dda05ae0fe7ac952ee634a8
yeeunseo-dev <anfrhrl96@gmail.com>
2024-05-30 15:39:09 +0900

    문제4번 미해결

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,32 @@
+import { useEffect, useState } from "react";
+
 // TODO: 이곳에 커스텀훅 작성하세요.
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        console.log("response:", response);
+        if (!response.ok) {
+          throw new Error("Network 오류");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
